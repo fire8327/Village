6dc434875c1eb59bf90f178c07aaaed3d5453e52
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-03-05 11:28:34 +0000

    create territory

---
+++ Assets/JS/slider.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper-territory', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.territory-next',
+      prevEl: '.territory-prev',
+    },
+});
+const swiper2 = new Swiper('.swiper-transport', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.transport-next',
+      prevEl: '.transport-prev',
+    },
+});
