c1ddea376c0544d692540d8be4b7e9eca441686f
fire8327 <fire83274@gmail.com>
2023-03-07 17:05:43 +0300

    create choice

--- Assets/JS/slider.js
+++ Assets/JS/slider.js
@@ -1,35 +1,37 @@
 const swiper = new Swiper('.swiper-territory', {
-    // Navigation arrows
     navigation: {
       nextEl: '.territory-next',
       prevEl: '.territory-prev',
     },
 });
 const swiper2 = new Swiper('.swiper-transport', {
-    // Navigation arrows
     navigation: {
       nextEl: '.transport-next',
       prevEl: '.transport-prev',
     },
 });
 const swiper3 = new Swiper('.swiper-tokyo', {
-    // Navigation arrows
     navigation: {
       nextEl: '.tokyo-next',
       prevEl: '.tokyo-prev',
     },
 });
 const swiper4 = new Swiper('.swiper-bergen', {
-    // Navigation arrows
     navigation: {
       nextEl: '.bergen-next',
       prevEl: '.bergen-prev',
     },
 });
 const swiper5 = new Swiper('.swiper-newyork', {
-    // Navigation arrows
     navigation: {
       nextEl: '.newyork-next',
       prevEl: '.newyork-prev',
     },
+});
+const swiper6 = new Swiper('.swiper-choice', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
