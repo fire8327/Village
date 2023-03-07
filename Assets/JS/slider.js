28c0ca1a4eb4593a4dc43a55f1a322b676ad8bcd
fire8327 <fire83274@gmail.com>
2023-03-07 16:32:27 +0300

    last block

--- Assets/JS/slider.js
+++ Assets/JS/slider.js
@@ -12,3 +12,24 @@ const swiper2 = new Swiper('.swiper-transport', {
       prevEl: '.transport-prev',
     },
 });
+const swiper3 = new Swiper('.swiper-tokyo', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.tokyo-next',
+      prevEl: '.tokyo-prev',
+    },
+});
+const swiper4 = new Swiper('.swiper-bergen', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.bergen-next',
+      prevEl: '.bergen-prev',
+    },
+});
+const swiper5 = new Swiper('.swiper-newyork', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.newyork-next',
+      prevEl: '.newyork-prev',
+    },
+});
