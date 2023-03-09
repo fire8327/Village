e3f25c4ff30d11f5c68c9d1bb9c033cfe5355e88
fire8327 <fire83274@gmail.com>
2023-03-09 16:04:49 +0300

    last block

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,25 +1,19 @@
-/* navigation */
 /*jshint esnext: true */
-const toggler = document.getElementById("toggler");
-const mNav = document.getElementById("mobile__nav");
-const close = document.getElementById("toggler__expanded");
-const overlay = document.getElementById("overlay");
-const body = document.getElementsByTagName("body")[0];
 
-toggler.addEventListener("click", () => {
-  mNav.classList.remove("translate-x-full");
-  overlay.classList.remove("hidden");
-  body.classList.add("overflow-hidden");
+/* tabs */
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+btn1.addEventListener("click", () => {
+  btn1.classList.add("btn-active");
+  btn2.classList.remove("btn-active");
+  img1.classList.remove("opacity-0");
+  img2.classList.add("opacity-0");
 });
-
-close.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden");
-  body.classList.remove("overflow-hidden"); 
-});
-
-overlay.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden"); 
-  body.classList.remove("overflow-hidden"); 
+btn2.addEventListener("click", () => {
+  btn1.classList.remove("btn-active");
+  btn2.classList.add("btn-active");
+  img2.classList.remove("opacity-0");
+  img1.classList.add("opacity-0");
 });
